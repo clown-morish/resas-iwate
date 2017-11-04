4ca8c803c4c2e4565ee07ccc8afab7d95ef474f3
Showki <g231o026@s.iwate-pu.ac.jp>
2017-11-04 22:49:02 +0900

    返す配列（JSON)の整形

--- app/Http/Controllers/PopulationController.php
+++ app/Http/Controllers/PopulationController.php
@@ -16,10 +16,9 @@ class PopulationController extends Controller
 
 	public function index()
 	{
-		$takizawa_pop = $this->getPopulation($this->iwate_cd,$this->takizawa_cd);
-		$tokyo_pop = $this->getPopulation($this->tokyo_cd,$this->sinjuku_cd);
-
-		dd($tokyo_pop);
+		$population['iwate'] = $this->getPopulation($this->iwate_cd,$this->takizawa_cd);
+		$population['tokyo'] = $this->getPopulation($this->tokyo_cd,$this->sinjuku_cd);
+		// return view('welcome',response()->json($population));
 	}
 
 	public function getPopulation($pref_cd, $city_cd)
@@ -32,8 +31,16 @@ class PopulationController extends Controller
 		$url = "https://opendata.resas-portal.go.jp/api/v1/townPlanning/commuteSchool/areaPopulationCircle?prefecture_cd=".$pref_cd."&city_cd=".$city_cd."&mode=2&year=2010";
 		$curl = curl_init(); 
 		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		$response = curl_exec($curl);
-		$result = json_decode($response, true);
+		$res = curl_exec($curl);
+		curl_close($curl);
+		$res_array = json_decode($res, true);
+
+		$result['noonDataSum'] = $res_array['result']['noonDataSum'];
+		$result['nightDataSum'] = $res_array['result']['nightDataSum'];
+		$result['dayNightRate'] = $res_array['result']['dayNightRate'];
+		
+		return $result;
 	}
 }
