283753929a50ca0619382abc1a8733d711c63bf6
TaikiFnit <taikifnitspg@gmail.com>
2017-11-05 14:57:59 +0900

    add rende_nature

--- public/js/render_chart.js
+++ public/js/render_chart.js
@@ -13,18 +13,18 @@ $(document).ready(() => {
   render()
 
   $('#iwate-city').change(function() {
-  console.log("fnit")
+    console.log("fnit")
     // 選択されているvalue属性値を取り出す
-  iwateCity = $('[name=iwateCity]').val()
-  render()
+    iwateCity = $('[name=iwateCity]').val()
+    render()
   });
 
   $('#tokyo-city').change(function() {
     console.log("fffni")
       // 選択されているvalue属性値を取り出す
-    tokyoCity = $('[name=tokyoCity]').val()
-    render()
-  });
+      tokyoCity = $('[name=tokyoCity]').val()
+      render()
+    });
 });
 
 function render() {
@@ -47,40 +47,69 @@ var render_population = () => {
 
     if( populationChart ){
      populationChart.destroy();
-     }
+   }
 
-    populationChart =  new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ["岩手-昼", "岩手-夜", "東京-昼", "東京-夜"],
-        datasets: [{
-          label: '中夜間人口比率',
-          data: [response["iwate"]["noonDataSum"], response["iwate"]["nightDataSum"], response["tokyo"]["noonDataSum"], response["tokyo"]["nightDataSum"]],
-          backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(90, 225, 132, 0.2)',
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(90, 225, 132, 0.2)'
-          ],
-          borderWidth: 1
+   populationChart =  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["岩手-昼", "岩手-夜", "東京-昼", "東京-夜"],
+      datasets: [{
+        label: '中夜間人口比率',
+        data: [response["iwate"]["noonDataSum"], response["iwate"]["nightDataSum"], response["tokyo"]["noonDataSum"], response["tokyo"]["nightDataSum"]],
+        backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(90, 225, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(90, 225, 132, 0.2)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
         }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero:true
-            }
-          }]
-        }
       }
-    });
+    }
   });
+ });
 }
 
 var render_nature = () => {
+    // http request
+    getJson("/Land", (response) => {
+      console.log(response)
 
-}
+      var ctx = document.getElementById("natureChart").getContext('2d');
+      const incomeChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ["東京", "岩手"],
+          datasets: [{
+            label: '自然の豊かさ（森林と野原の量）',
+            data: [response["tokyo"]["statearea"], response["iwate"]["statearea"]],
+            backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(90, 225, 132, 0.2)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      });
+    })
+  }
 
 var render_income = () => {
   // http request
@@ -91,33 +120,33 @@ var render_income = () => {
 
     if( incomeChart ){
      incomeChart.destroy();
-    }
+   }
 
-    incomeChart =  new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ["東京", "岩手"],
-        datasets: [{
-          label: ' 一人あたりの賃金',
-          data: [response["tokyo"]["value"], response["iwate"]["value"]],
-          backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(90, 225, 132, 0.2)'
-          ],
-          borderWidth: 1
+   incomeChart =  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["東京", "岩手"],
+      datasets: [{
+        label: ' 一人あたりの賃金',
+        data: [response["tokyo"]["value"], response["iwate"]["value"]],
+        backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(90, 225, 132, 0.2)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
         }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero:true
-            }
-          }]
-        }
       }
-    });
-  })
+    }
+  });
+ })
 }
 
 // Http Request
